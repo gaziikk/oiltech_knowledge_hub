90304102fdb106e4079569315a71db19afaaeb7f
gaziikk <160274798+gaziikk@users.noreply.github.com>
2024-02-16 22:40:17 +0300

    contacts
    The contacts directory contains the logic for sending messages to the mail and telegram bot

---
+++ contacts/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from oiltech_knowledge_hub.forms import AddPostForm
+from django.core.mail import EmailMessage
+from os import getenv
+from aiogram import Bot, asyncio
+
+# Контакты
+def contacts(request):
+    message_send = False
+    
+    if request.method == 'POST':
+        form = AddPostForm(request.POST)
+        if form.is_valid():
+            full_name = form.cleaned_data['full_name']
+            email = form.cleaned_data['email']
+            answer = form.cleaned_data['answer']
+
+            subject = f'Новый вопрос от пользователя {full_name}'
+            message = (f'База знаний в области нефтяных технологий.\n'
+                       f'От: {full_name}\n'
+                       f'Почта: ({email}):\n\n'
+                       f'{answer}')
+
+            # Отправка сообщений в телеграм
+            async def send_telegram_message():
+                bot = Bot(token=getenv('TOKEN'))
+                
+                await bot.send_message(getenv('TELEGRAM_ID'),
+                           f'*{subject}*\n'
+                           f'{message}',
+                           parse_mode='MARKDOWN'
+                          )
+            asyncio.run(send_telegram_message())
+                
+            # Отправка сообщений на почту
+            email_send = EmailMessage(
+                subject,
+                message,
+                str(getenv("EMAIL")),
+                to=[str(getenv("EMAIL"))]
+            )
+            email_send.send(fail_silently=False)
+            message_send = True
+    else:
+        form = AddPostForm()
+    data = {
+        'form': form,
+        'message_send': message_send
+    }
+    return render(request, 'contacts.html', data)
